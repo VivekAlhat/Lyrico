b41f275b1672c20e878e910fd3ac4e8d0b8ab27f
VivekAlhat <vivekalhat14@outlook.com>
2020-03-20 15:43:26 +0530

    Pushed CLI version

---
+++ main.py
@@ -0,0 +1,27 @@
+import re
+import json
+import urllib3
+
+# Disable all HTTP warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Connection pool
+http = urllib3.PoolManager()
+
+# Getting input
+artist = input("Artist: ")
+artistList = re.split(r'\s|-', artist)
+artist = "-".join(artistList)
+title = input("Track: ")
+titleList = re.split(r'\s|-', title)
+title = "-".join(titleList)
+
+
+def fetchLyrics(artist, title):
+    url = "https://api.lyrics.ovh/v1/{}/{}".format(artist, title)
+    resp = http.request('GET', url)
+    apiResponse = json.loads(resp.data.decode('utf8'))
+    print(apiResponse['lyrics'])
+    # print(type(apiResponse['lyrics']))
+
+fetchLyrics(artist, title)
